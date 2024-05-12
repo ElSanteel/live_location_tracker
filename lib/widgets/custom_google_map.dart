356bc4778fe4ef9e8adb27bd687e16667f4dbbd7
ElSanteel <alsantel.ma@gmail.com>
2024-05-13 00:26:49 +0300

    refactor code

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_with_google_maps/utils/location_service.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
@@ -15,17 +16,18 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
 
   // we have made the googleMapController nullable because we are sure that it will be initialized in the onMapCreated callback
   GoogleMapController? googleMapController;
-  late Location location;
   // we have made markers set
   Set<Marker> markers = {};
+  // we have made locationService object
+  late LocationService locationService;
 
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
         zoom: 17, target: LatLng(31.187084851056554, 29.928110526889437));
 
-    location = Location();
-
+    // we have initialized the locationService object
+    locationService = LocationService();
     updateMyLocation();
     super.initState();
   }
@@ -51,100 +53,46 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     googleMapController!.setMapStyle(nightMapStyle);
   }
 
-  // step 1 : method to check if location service is enabled or not..
-  Future<void> checkAndRequestLocationService() async {
-    // check if location service is enabled or not
-    var isServiceEnabled = await location.serviceEnabled();
-    if (!isServiceEnabled) {
-      // request location service
-      isServiceEnabled = await location.requestService();
-      if (!isServiceEnabled) {
-        // TODO: show error bar
-      }
-    }
-  }
-
-  // // step 2 : method to check if location permission is granted or not ( first way )
-  // Future<bool> checkAndRequestLocationPermission() async {
-  //   // check if location permission is granted or not
-  //   var permissionStatus = await location.hasPermission();
-  //   if (permissionStatus == PermissionStatus.denied) {
-  //     // request location permission
-  //     permissionStatus = await location.requestPermission();
-  //     if (permissionStatus != PermissionStatus.granted) {
-  //       return false;
-  //     } else {
-  //       return true;
-  //     }
-  //   } else {
-  //     if (permissionStatus == PermissionStatus.deniedForever) {
-  //       return false;
-  //     } else {
-  //       return true;
-  //     }
-  //   }
-  // }
-
-  // step 2 : method to check if location permission is granted or not ( second way )
-  Future<bool> checkAndRequestLocationPermission() async {
-    // check if location permission is granted or not
-    var permissionStatus = await location.hasPermission();
-    // check if the permission is denied forever
-    if (permissionStatus == PermissionStatus.deniedForever) {
-      return false;
-    }
-    // check if the permission is denied
-    if (permissionStatus == PermissionStatus.denied) {
-      // request location permission
-      permissionStatus = await location.requestPermission();
-      // check if the permission is not granted
-      if (permissionStatus != PermissionStatus.granted) {
-        // return false if the permission is not granted and the user denied the permission
-        return false;
-      }
-    }
-    // return true if the permission is granted and the user allowed the permission
-    return true;
-  }
-
-  // step 3 : method to get the user location data
-  void getLocationData() {
-    location.changeSettings(distanceFilter: 2);
-
-    location.onLocationChanged.listen(
-      (locationData) {
-        // create a camera position object with the user location
-        var cameraPosition = CameraPosition(
-            target: LatLng(locationData.latitude!, locationData.longitude!),
-            zoom: 12);
-
-        // create a marker for the user location
-        var myLocationMarker = Marker(
-          markerId: const MarkerId('my_location_marker'),
-          position: LatLng(locationData.latitude!, locationData.longitude!),
-        );
-
-        // add the marker to the markers set
-        markers.add(myLocationMarker);
-
-        // update the state to rebuild the widget cuz animateCamera method will not rebuild the widget , it just change the position of the camera
-        setState(() {});
-
-        // animate the camera to the user location
-        // ? -> null safety operator used here to check if the googleMapController is null or not
-        // so if the googleMapController is null the animateCamera method will not be called
-        googleMapController
-            ?.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-      },
-    );
-  }
-
   void updateMyLocation() async {
-    await checkAndRequestLocationService();
-    var hasPermission = await checkAndRequestLocationPermission();
+    await locationService.checkAndRequestLocationService();
+    var hasPermission =
+        await locationService.checkAndRequestLocationPermission();
     if (hasPermission) {
-      getLocationData();
+      locationService.getRealTimeLocationData((locationData) {
+        // set the user location marker
+        setMyLocationMarker(locationData);
+
+        // set the camera position to the user location
+        setMyCameraPosition(locationData);
+      });
     } else {}
+  }
+
+  void setMyCameraPosition(LocationData locationData) {
+    // create a camera position object with the user location
+    var cameraPosition = CameraPosition(
+        target: LatLng(locationData.latitude!, locationData.longitude!),
+        zoom: 12);
+
+    // animate the camera to the user location
+    // ? -> null safety operator used here to check if the googleMapController is null or not
+    // so if the googleMapController is null the animateCamera method will not be called
+    googleMapController
+        ?.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
+  void setMyLocationMarker(LocationData locationData) {
+    // create a marker for the user location
+    var myLocationMarker = Marker(
+      markerId: const MarkerId('my_location_marker'),
+      position: LatLng(locationData.latitude!, locationData.longitude!),
+    );
+
+    // add the marker to the markers set
+    markers.add(myLocationMarker);
+
+    // update the state to rebuild the widget cuz animateCamera method will not rebuild the widget , it just change the position of the camera
+    setState(() {});
   }
 }
 
